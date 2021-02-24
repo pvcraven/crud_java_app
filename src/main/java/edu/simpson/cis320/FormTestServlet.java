e5f6ed7466aaba2deefdb27768517fc588a1d15f
Paul V Craven <paul@cravenfamily.com>
2021-02-24 08:30:19 -0600

    Update

---
+++ src/main/java/edu/simpson/cis320/FormTestServlet.java
@@ -0,0 +1,42 @@
+package edu.simpson.cis320;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "FormTestServlet", value = "/api/form_test_servlet")
+public class FormTestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // You can output in any format, text/JSON, text/HTML, etc. We'll keep it simple
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+
+        // Print that this is a get, not a post
+        out.println("Get");
+
+        // Grab the data we got via a parameter
+        String fieldname = request.getParameter("fieldname");
+
+        // Just print the data out to confirm we got it.
+        out.println("fieldname='"+fieldname+"'");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // You can output in any format, text/JSON, text/HTML, etc. We'll keep it simple
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+
+        // Print that this is a post
+        out.println("Post");
+
+        // Grab the data we got via a parameter
+        String fieldname = request.getParameter("fieldname");
+
+        // Just print the data out to confirm we got it.
+        out.println("fieldname='"+fieldname+"'");
+    }
+}
